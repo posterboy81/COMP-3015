db986fdf33eb9fdc42b7656da5fc412e45315352
Matthew Simpson <45255318+posterboy81@users.noreply.github.com>
2022-06-27 00:32:34 -0700

    commented testing logic

--- 05/main.php
+++ 05/main.php
@@ -6,20 +6,23 @@ include('./classes/Course.php');
 include('./classes/io/RemoteReadWriter.php');
 include('./classes/io/LocalReadWriter.php');
 
+
+// new course manager
 $StudentMatthew = new CourseManager('Matthew', new LocalReadWriter());
 
+// two new courss 
 $bcit3015 = new Course("COMP 3015", true, "comp3015");
 $bcit3012 = new Course("COMP 3012", true, "comp3012");
 
-echo "Course Info: " . $bcit3015->toString() . "\n";
-
+// add the coures to the manager
 $StudentMatthew->addCourse($bcit3015);
 $StudentMatthew->addCourse($bcit3012);
 
+// get the students name
 echo "Student Name Is: " . $StudentMatthew->getName() . "\n";
 
+//print the students course info 
 echo "Student Course Info: \n";
-
 foreach ($StudentMatthew->getCourses() as $c) {
     echo $c->toString() . "\n";
 }
