029fd0a87929b4bc6f72a7d0679a4b1838d30d75
Matthew Simpson <45255318+posterboy81@users.noreply.github.com>
2022-06-30 19:43:11 -0700

    2

---
+++ 05/courselist.php
@@ -0,0 +1,19 @@
+    
+    
+    
+    <!-- <fieldset>
+         <input type="checkbox" name="comp3012" id="comp3012">
+         <label for="comp3012">COMP 3012</label>
+         <input type="button" name="delete3012" id="delete3012" class="button delbutton" value="DELETE">
+     </fieldset>
+     <fieldset>
+         <input type="checkbox" name="comp3015" id="comp3015">
+         <label for="comp3015">COMP 3015</label>
+         <input type="button" name="delete3015" id="delete3015" class="button delbutton" value="DELETE">
+     </fieldset>
+     <fieldset>
+         <input type="checkbox" name="comp3012" id="comp3012">
+         <label for="comp3603">COMP 3603</label>
+         <input type="button" name="delete3603" id="delete3603" class="button delbutton" value="DELETE">
+     </fieldset> -->
+
--- 05/index.php
+++ 05/index.php
@@ -1,8 +1,9 @@
 <?php include "header.php" ?>
 
+
 <course-search>
-    <form>
-        <h2>Course Search:</h2>
+    <form method="post">
+        <h2>Course input:</h2>
         <input type="text" name="course" id="course"></input>
         <input type="submit" name="add" id="add" value="ADD" class="button addbutton"></input>
     </form>
@@ -11,22 +12,8 @@
     <form>
         <h2>My Courses</h2>
 
+        <?php include "courseList.php" ?>
 
-        <!-- <fieldset>
-            <input type="checkbox" name="comp3012" id="comp3012">
-            <label for="comp3012">COMP 3012</label>
-            <input type="button" name="delete3012" id="delete3012" class="button delbutton" value="DELETE">
-        </fieldset>
-        <fieldset>
-            <input type="checkbox" name="comp3015" id="comp3015">
-            <label for="comp3015">COMP 3015</label>
-            <input type="button" name="delete3015" id="delete3015" class="button delbutton" value="DELETE">
-        </fieldset>
-        <fieldset>
-            <input type="checkbox" name="comp3012" id="comp3012">
-            <label for="comp3603">COMP 3603</label>
-            <input type="button" name="delete3603" id="delete3603" class="button delbutton" value="DELETE">
-        </fieldset> -->
     </form>
 </course-list>
 <?php include "footer.php" ?>
